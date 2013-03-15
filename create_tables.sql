357d63f5eea926c089442383bf97cc1a614c9927
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2013-03-15 17:36:09 -0300

    versão inicial

---
+++ create_tables.sql
@@ -0,0 +1,102 @@
+﻿DROP TABLE empresarios FORCE;
+DROP TABLE clubes FORCE;
+DROP TABLE vendas FORCE;
+DROP TABLE series FORCE;
+DROP TABLE jogadores FORCE;
+DROP TABLE goleiros FORCE;
+DROP TABLE zagueiros FORCE;
+DROP TABLE laterais FORCE;
+DROP TABLE volantes FORCE;
+DROP TABLE meio_Ofensivos FORCE;
+DROP TABLE atacantes FORCE;
+DROP TABLE emprestimos FORCE;
+
+CREATE TABLE empresarios OF Empresario(
+    CONSTRAINT cpf_cnpj_pk PRIMARY KEY(cpf_cnpj),
+    CONSTRAINT nome_completo_empresario_cons CHECK(nome_completo is NOT NULL),
+    CONSTRAINT telefones_cons CHECK(telefones is NOT NULL),
+    CONSTRAINT emails_cons CHECK(emails is NOT NULL)
+);
+
+CREATE TABLE clubes OF Clube(
+    CONSTRAINT nome_completo_cons_clube CHECK(nome_completo is NOT NULL),
+    CONSTRAINT nome_comum_cons_clube CHECK(nome_comum is NOT NULL),
+    CONSTRAINT serie_atual_cons CHECK(serie_atual is NOT NULL),
+    CONSTRAINT data_fundacao_cons CHECK(data_fundacao is NOT NULL),
+    CONSTRAINT num_titulos_cons CHECK(num_titulos_por_serie is NOT NULL),
+    CONSTRAINT nome_estadio_cons CHECK(nome_estadio is NOT NULL)
+);
+
+CREATE TABLE vendas OF Venda(
+    CONSTRAINT clube_vendedor_cons CHECK(clube_vendedor is NOT NULL),
+    CONSTRAINT clube_comprador_cons CHECK(clube_comprador IS NOT NULL),
+    CONSTRAINT data_cons CHECK(data is NOT NULL),
+    CONSTRAINT valor_de_venda_cons CHECK(valor_de_venda > 0),
+    CONSTRAINT jogador_vendido_cons CHECK(jogador_vendido is NOT NULL),
+    CONSTRAINT alteracao_valor_jogador_cons CHECK(alteracao_valor_jogador > 0)
+);
+
+CREATE TABLE series OF Serie(
+    CONSTRAINT nome_completo_serie_cons CHECK(nome_completo is NOT NULL),
+    CONSTRAINT apelido_serie_cons CHECK(apelido is NOT NULL),
+    CONSTRAINT regulamento_cons CHECK(regulamento is NOT NULL)
+);
+
+CREATE TABLE jogadores OF Jogador(
+    CONSTRAINT nome_completo_jogador_cons CHECK(nome_completo is NOT NULL),
+    CONSTRAINT apelido_jog_cons CHECK(apelido is NOT NULL),
+    CONSTRAINT situacao_medica_cons CHECK(situacao_medica is NOT NULL),
+    CONSTRAINT tipo_situacao_medica CHECK (UPPER(situacao_medica) IN ('CONTUNDIDO', 'VOLTANDO-CONTUSAO',   'SAUDAVEL')),
+    CONSTRAINT clube_dono_cons CHECK(clube_dono is NOT NULL),
+    CONSTRAINT clube_atual_cons CHECK(clube_atual is NOT NULL),
+    CONSTRAINT empresario_resp_cons CHECK(empresario_responsavel is NOT NULL),
+    CONSTRAINT valor_cons CHECK(valor > 0),
+    CONSTRAINT status_cons CHECK(status is NOT NULL),
+    CONSTRAINT tipo_status CHECK (UPPER(status) IN ('A-VENDA', 'LIBERADO',   'COM-CONTRATO')),
+    CONSTRAINT total_jogos_cons CHECK(total_jogos > 0),
+    CONSTRAINT gols_marcados_cons CHECK(gols_marcados is NOT NULL),
+    CONSTRAINT faltas_cometidas_cons CHECK(faltas_cometidas > 0),
+    CONSTRAINT faltas_sofridas_cons CHECK(faltas_sofridas > 0),
+    CONSTRAINT cartoes_amarelo_cons CHECK(cartoes_amarelo > 0),
+    CONSTRAINT cartoes_vermelho_cons CHECK(cartoes_vermelho > 0),
+    CONSTRAINT data_nascimento_cons CHECK(data_nascimento IS NOT NULL)
+);
+
+
+CREATE TABLE emprestimos OF Emprestimo(
+    CONSTRAINT data_emprestimo_cons CHECK(data is NOT NULL),
+    CONSTRAINT jogador_emprestado_cons CHECK(jogador_emprestado is NOT NULL)
+);
+
+
+CREATE TABLE goleiros OF Goleiro(
+    CONSTRAINT num_defesas_cons CHECK(num_defesas_dificeis > 0),
+    CONSTRAINT gols_sofridos_cons CHECK(gols_sofridos > 0)
+);
+
+
+CREATE TABLE zagueiros OF Zagueiro
+    (CONSTRAINT num_desarmes_zagueiro_cons CHECK(num_desarmes > 0)
+);
+
+CREATE TABLE laterais OF Lateral
+    (CONSTRAINT num_desarmes_lateral_cons CHECK(num_desarmes > 0),
+    CONSTRAINT cruzamentos_feitos_cons CHECK(cruzamentos_feitos > 0),
+    CONSTRAINT cruzamentos_certos_cons CHECK(cruzamentos_certos <= cruzamentos_feitos)
+);
+
+CREATE TABLE volantes OF Volante
+    (CONSTRAINT num_desarmes_volante_cons CHECK(num_desarmes > 0),
+    CONSTRAINT passes_realizados_cons CHECK(passes_realizados > 0),
+    CONSTRAINT passes_feitos_cons CHECK(passes_feitos <= passes_realizados)
+);
+
+CREATE TABLE meio_ofensivos OF Meio_Ofensivo
+    (CONSTRAINT num_passes_cons CHECK(num_passes_feitos > 0),
+    CONSTRAINT assistencias_cons CHECK(assistencias <= num_passes_feitos)
+);
+
+CREATE TABLE atacantes OF Atacante
+    (CONSTRAINT num_finalizacao_cons CHECK(num_finalizacao > gols_marcados)
+);
+​
---
+++ create_types.sql
@@ -0,0 +1,136 @@
+﻿DROP TYPE Jogador FORCE;
+DROP TYPE Endereco FORCE;
+DROP TYPE Emprestimo FORCE;
+DROP TYPE Serie FORCE;
+DROP TYPE Venda FORCE;
+DROP TYPE Clube FORCE;
+DROP TYPE listaTelefone_array FORCE;
+DROP TYPE listaEmail_array FORCE;
+DROP TYPE Empresario FORCE;
+DROP TYPE Atacante FORCE;
+DROP TYPE Meio_Ofensivo FORCE;
+DROP TYPE Volante FORCE;
+DROP TYPE Goleiro FORCE;
+DROP TYPE Lateral FORCE;
+DROP TYPE Zagueiro FORCE;
+DROP TYPE LISTADECLUBESDOJOGADOR_ARRAY FORCE;
+
+CREATE OR REPLACE TYPE listaTelefone_array AS VARRAY(100) OF VARCHAR2(40);
+/
+CREATE OR REPLACE TYPE listaEmail_array AS VARRAY(100) OF VARCHAR2(40);
+/
+
+CREATE OR REPLACE TYPE Endereco AS OBJECT(
+    rua VARCHAR2(50),
+    bairro VARCHAR2(50),
+    complemento VARCHAR2(50),
+    cep VARCHAR2(10),
+    cidade VARCHAR2(50)
+);
+/
+
+CREATE OR REPLACE TYPE Serie AS OBJECT(
+    nome_completo VARCHAR2(40),
+    apelido VARCHAR2(30),
+    regulamento VARCHAR2(4000)
+);
+/
+
+CREATE OR REPLACE TYPE Clube AS OBJECT(
+    nome_completo VARCHAR2(40),
+    nome_comum VARCHAR2(30),
+    serie_atual Serie,
+    data_fundacao DATE,
+    num_titulos_por_serie VARCHAR2(100),
+    nome_estadio VARCHAR2(50)
+);
+/
+
+CREATE OR REPLACE TYPE Empresario AS OBJECT(
+    nome_completo VARCHAR2(50),
+    apelido VARCHAR(30),
+    cpf_cnpj VARCHAR2(40),
+    telefones listaTelefone_array,
+    emails listaEmail_array
+);
+/
+
+CREATE OR REPLACE TYPE Jogador AS OBJECT(
+    nome_completo VARCHAR2(40),
+    apelido VARCHAR2(30),
+    situacao_medica VARCHAR2(20),
+    clube_dono  Clube,
+    clube_atual Clube,
+    empresario_responsavel REF Empresario,
+    valor NUMBER,
+    status VARCHAR2(20),
+    total_jogos INTEGER,
+    gols_marcados INTEGER,
+    faltas_cometidas INTEGER,
+    faltas_sofridas INTEGER,
+    cartoes_amarelo INTEGER,
+    cartoes_vermelho INTEGER,
+    data_nascimento DATE
+       
+) NOT FINAL;
+/
+
+CREATE OR REPLACE TYPE Venda AS OBJECT(
+    clube_vendedor REF Clube,
+    clube_comprador REF Clube,
+    data DATE,
+    valor_de_venda NUMBER,
+    alteracao_valor_jogador NUMBER,
+    jogador_vendido REF JOGADOR
+
+
+);
+/
+
+CREATE OR REPLACE TYPE Emprestimo AS OBJECT(
+    data DATE,
+    jogador_emprestado REF JOGADOR
+);
+/
+
+CREATE OR REPLACE TYPE Atacante under Jogador(
+    num_finalizacao INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE Meio_Ofensivo UNDER Jogador(
+    num_passes_feitos INTEGER,
+    assistencias INTEGER
+   
+);
+/
+
+CREATE OR REPLACE TYPE Volante UNDER Jogador(
+    num_desarmes INTEGER,
+    passes_realizados INTEGER,
+    passes_feitos INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE Lateral UNDER Jogador(
+    num_desarmes INTEGER,
+    cruzamentos_feitos INTEGER,
+    cruzamentos_certos INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE Zagueiro UNDER Jogador(
+    num_desarmes INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE Goleiro UNDER Jogador(
+    num_defesas_dificeis INTEGER,
+    gols_sofridos INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE listaDeClubesDoJogador_array AS VARRAY(100) OF Clube;
+/
+ALTER TYPE Jogador ADD ATTRIBUTE lista_clubes listaDeClubesDoJogador_array CASCADE;
+/
